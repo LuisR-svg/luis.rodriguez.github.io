a96142d1dc628fc020c02f84cab35b104cd0851f
LuisR-svg <53505135+LuisR-svg@users.noreply.github.com>
2023-04-04 15:24:11 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
 require "../app/core/init.php";
 $controller=$_GET['page_name'] ?? "home";
-show($controller)
+$controller=strtolower($controller);
+if(file_exists("../app/controllers/" . $controller . ".php")){
+    require "../app/controllers/" . $controller . ".php"
+}
+else{
+    echo'controller not found'
+}
